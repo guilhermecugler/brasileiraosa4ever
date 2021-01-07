ee9fb7746377a1abf2ed4ffa9ecc0ce502ce1ac4
Guilherme Cugler <guilhermecugler@gmail.com>
2021-01-07 08:56:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -27,6 +27,47 @@ for (var i = 0; i < headers.length; i++) {
 }
     };
     req.send();
-	}
+    }
 })
 
+
+
+$(function() {
+	function createCORSRequest(method, url){
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr){
+        xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != "undefined"){
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+    } else {
+        xhr = null;
+    }
+    return xhr;
+	}
+  var req = createCORSRequest("GET", "https://cors-anywhere.herokuapp.com/https://tournamentscheduler.net/schedule/MTA4MTIyMDc3NzE/results");
+  if (req) {
+  	req.onload = function(data) {
+    	console.log(data);
+      var full = data.target.response;
+      $("#target2").html($(full).find(".schedule  "));
+      
+      var headers = document.getElementsByTagName("th"); 
+for (var i = 0; i < headers.length; i++) {
+  headers[i].innerHTML = headers[i].innerHTML.replace("Player", "Clã")
+                           .replace("Played", "Partidas Jogadas")
+                           .replace("Wins", "Vitórias")
+                           .replace("Draws", "Empates")
+                           .replace("Losses", "Derrotas")
+                           .replace("Points", "Pontos")
+                           ;
+}
+
+$('table tr').find('td:eq(6),th:eq(6)').remove();
+$('table tr').find('td:eq(6),th:eq(6)').remove();
+
+
+    };
+    req.send();
+    }
+})
